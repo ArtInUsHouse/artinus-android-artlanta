e59a62c6aed770a2a79137888279649dc77c2043
godfatherfrancis <francis.aarons@gmail.com>
2016-11-15 16:32:32 -0500

    update store & location activity

--- sample/src/main/java/fr/rolandl/sample/carousel/Store.java
+++ sample/src/main/java/fr/rolandl/sample/carousel/Store.java
@@ -22,7 +22,7 @@ public class Store extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
+        setContentView(R.layout.activity_store);
         Log.i(TAG, "onCreate");
         setTitle("Shop ARTlanta");
 
